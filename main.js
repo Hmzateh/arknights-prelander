0c568367a102c92842c3546f41821a11dbc87f97
Hmzateh <hamza.talebelhana@gmail.com>
2026-02-10 05:00:56 +0100

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// Main JS - Arknights: Endfield Prelander
-// Ready for future enhancements (tracking, animations, etc.)
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Page loaded
-});
